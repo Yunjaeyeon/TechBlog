a70f5e7ceb3febdf146b4f8385d99e1f38d219c6
jaeYeon <A23670@nis.co.kr>
2024-05-04 19:53:31 +0900

    [FEAT] CallByValue, CallByReference 추가

--- src/main/java/chap3/Animal.java
+++ src/main/java/chap3/Animal.java
@@ -3,6 +3,7 @@ package chap3;
 public class Animal {
     public String name;
 
+
    public void showName() {
        System.out.printf("Hi, My Name is %s\n", name);
    }
---
+++ src/main/java/chap3/reference/CallByValue.java
@@ -0,0 +1,13 @@
+package chap3.reference;
+
+public class CallByValue {
+    public static void main(String[] args){
+        int a = 10;
+        int b = a;
+
+        b = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
