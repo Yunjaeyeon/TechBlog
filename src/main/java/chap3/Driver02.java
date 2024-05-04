ff953fe918572e97df1585be9c3585b87b1b0b94
jaeYeon <A23670@nis.co.kr>
2024-05-04 17:40:03 +0900

    [FEAT] Driver02추가

---
+++ src/main/java/chap3/Driver02.java
@@ -0,0 +1,11 @@
+package chap3;
+
+public class Driver02 {
+    public static void main(String[] args){
+        Animal animal = new Animal();
+        Animal bird = new Bird();
+
+        animal.showMe();
+        bird.showMe();
+    }
+}
